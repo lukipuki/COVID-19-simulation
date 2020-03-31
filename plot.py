d7646f4162953ea93da5e9c4159607610dd8c008
Lukáš Poláček <lukas@polacek.email>
2020-03-31 16:29:32 +0200

    Graph now with dates in the tooltip

--- plot.py
+++ plot.py
@@ -8,13 +8,18 @@ from datetime import datetime, timedelta
 
 positive = [0,  0,  0, 0,  1,  2,  2, 2,  0,  3,  11, 11, 12, 17,
             11, 25, 8, 19, 13, 41, 7, 19, 12, 10, 43, 23, 22, 22, 27]
-accumulated = list(filter(lambda c: c >= 10, itertools.accumulate(positive)))
-# date_list = [datetime(2020, 3, 11) + timedelta(days=d) for d in range(len(accumulated))]
+prefix_length = 9
+accumulated = list(itertools.accumulate(positive))[prefix_length:]
 
-x = np.arange(1, len(accumulated) + 1)
-y_polynomial = 10 * (x ** 1.19)
-# To add exponential decay, do something like this
-# y_polynomial = 10 * (x ** 1.3) * np.exp(-x / 40)
+x = np.arange(1, len(accumulated) + 10)
+y_power_law = 10 * x ** 1.20
+
+# To add exponential decay, do something like this instead:
+# x = np.arange(1, len(accumulated) + 50)
+# y_power_law = (x ** 6.23) * np.exp(-x / 8.5) * 10**(-5)
+
+date_list = [(datetime(2020, 3, 1) + timedelta(days=int(d + prefix_length))) for d in x]
+date_list = [d.strftime('%Y-%m-%d') for d in date_list]
 
 layout = go.Layout(
     xaxis=dict(
@@ -35,6 +40,7 @@ figure.add_trace(
     go.Scatter(
         x=x,
         y=accumulated,
+        text=date_list,
         mode='lines+markers',
         name="Cases in Slovakia",
         line={'width': 3},
@@ -45,9 +51,11 @@ figure.add_trace(
 figure.add_trace(
     go.Scatter(
         x=x,
-        y=y_polynomial,
+        y=y_power_law,
+        text=date_list,
         mode='lines',
-        name=r'$\text{Power law fit: }10 \cdot t^{1.19}$',
+        # name=r'$\text{Power law fit: } t^{6.23} / 1.125^t \cdot 0.000019$',
+        name=r'$\text{Power law fit: } 10 \cdot t^{1.2}$',
         line={'width': 3},
     )
 )

8adffb1cae58fddb16223b9e0eedbe492b648166
Lukáš Poláček <lukas@polacek.email>
2020-04-03 22:21:46 +0200

    Fix power law graphs

--- graphs/power_law.py
+++ graphs/power_law.py
@@ -9,7 +9,8 @@ import sys
 import yaml
 from collections import namedtuple
 
-Country = namedtuple('Country', ['name', 'fit', 'fit_description', 'case_count'])
+Country = namedtuple('Country', ['name', 'formula', 'case_count'])
+Formula = namedtuple('Formula', ['lambd', 'text'])
 
 parser = argparse.ArgumentParser(description='COVID-19 power law visualization')
 parser.add_argument('data', metavar='data', type=str, help=f"YAML file with data")
@@ -17,19 +18,17 @@ parser.add_argument('country', metavar='country', type=str, help=f"Country")
 args = parser.parse_args()
 
 
-def TG_formula(t, TG, A):
-    return (A / TG) * ((t / TG)**6.23) / np.exp(t / TG)
-
-
-def TG_formula_tex(TG, A):
-    return r'$\frac{A}{TG} \cdot \left(\frac{t}{TG}\right)^{6.23} / e^{t/TG}$'
+def TG_formula(TG, A):
+    text = r'$\frac{_A}{_TG} \cdot \left(\frac{t}{_TG}\right)^{6.23} / e^{t/_TG}$'
+    text = text.replace("_A", f"{A}").replace("_TG", f"{TG}")
+    return Formula(lambda t: (A / TG) * ((t / TG)**6.23) / np.exp(t / TG), text)
 
 
 countries = [
-    Country('Slovakia', lambda t: 10 * t**1.21, r'$10 \cdot t^{1.2}$', 10),
-    Country('Italy', lambda t: TG_formula(t, 7.8, 4417), TG_formula_tex(7.8, 4417), 200),
-    Country('Spain', lambda t: TG_formula(t, 6.4, 3665), "A", 2500),
-    Country('Germany', lambda t: TG_formula(t, 6.7, 3773), "B", 2500)
+    Country('Slovakia', Formula(lambda t: 10 * t**1.21, r'$10 \cdot t^{1.2}$'), 10),
+    Country('Italy', TG_formula(7.8, 4417), 200),
+    Country('Spain', TG_formula(6.4, 3665), 2500),
+    Country('Germany', TG_formula(6.7, 3773), 2500)
 ]
 # Country('Italy', 0.48 * x ** 3.35, r'$0.5382 \cdot t^{3.37}$')]
 country = next(c for c in countries if c.name == args.country)
@@ -48,7 +47,7 @@ with open(args.data, 'r') as stream:
 cumulative_active = list(filter(lambda x: x >= country.case_count, itertools.accumulate(active)))
 date_list = date_list[len(active) - len(cumulative_active):]
 x = np.arange(1, len(cumulative_active) + 50)
-y_power_law = country.fit(x)
+y_power_law = country.formula.lambd(x)
 
 last_date = datetime.strptime(date_list[-1], '%Y-%m-%d')
 date_list += [(last_date + timedelta(days=d)).strftime('%Y-%m-%d') for d in range(1, 51)]
@@ -76,7 +75,7 @@ figure.add_trace(
         y=y_power_law,
         text=date_list,
         mode='lines',
-        name=country.fit_description,
+        name=country.formula.text,
         line={
             'dash': 'dash',
             'width': 3

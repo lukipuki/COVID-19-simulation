e26ddfc3bdf524ba9551cebad5d816a6fb93a926
Lukáš Poláček <lukas@polacek.email>
2020-04-05 12:25:00 +0200

    Put dates on x-axis when in Normal GraphType
    Also use 100 characters in a line, it's not the 1980's anymore.

--- graphs/growth.py
+++ graphs/growth.py
@@ -3,9 +3,7 @@ from plotly import offline
 from plotly.graph_objs import Figure, Layout, Scatter
 from datetime import datetime, timedelta
 import argparse
-import itertools
 import numpy as np
-import plotly.graph_objs as go
 import sys
 import yaml
 import os
@@ -18,16 +16,9 @@ from enum import Enum
 Country = namedtuple('Country', ['name', 'formula', 'case_count'])
 Formula = namedtuple('Formula', ['lambd', 'text'])
 
-parser = argparse.ArgumentParser(
-    description='COVID-19 country growth visualization')
-parser.add_argument('data',
-                    metavar='data',
-                    type=str,
-                    help=f"Directory with YAML files")
-parser.add_argument('country',
-                    metavar='country',
-                    type=str,
-                    help=f"Country name or 'ALL'")
+parser = argparse.ArgumentParser(description='COVID-19 country growth visualization')
+parser.add_argument('data', metavar='data', type=str, help=f"Directory with YAML files")
+parser.add_argument('country', metavar='country', type=str, help=f"Country name or 'ALL'")
 args = parser.parse_args()
 
 
@@ -40,13 +31,11 @@ class GraphType(Enum):
 def TG_formula(TG, A):
     text = r'$\frac{_A}{_TG} \cdot \left(\frac{t}{_TG}\right)^{6.23} / e^{t/_TG}$'
     text = text.replace("_A", f"{A}").replace("_TG", f"{TG}")
-    return Formula(lambda t: (A / TG) * ((t / TG)**6.23) / np.exp(t / TG),
-                   text)
+    return Formula(lambda t: (A / TG) * ((t / TG)**6.23) / np.exp(t / TG), text)
 
 
 countries = [
-    Country('Slovakia', Formula(lambda t: 8 * t**1.28, r'$8 \cdot t^{1.28}$'),
-            10),
+    Country('Slovakia', Formula(lambda t: 8 * t**1.28, r'$8 \cdot t^{1.28}$'), 10),
     Country('Italy', TG_formula(7.8, 4417), 200),
     # Country('Italy', Formula(lambda t: 0.5382 * t**3.37, r'$0.5382 \cdot t^{3.37}$'), 200),
     Country('Spain', TG_formula(6.4, 3665), 200),
@@ -68,62 +57,64 @@ class CountryData:
                 data = yaml.safe_load(stream)
                 positive = np.array([point['positive'] for point in data])
                 self.dead = np.array([point['dead'] for point in data])
-                self.recovered = np.array(
-                    [point['recovered'] for point in data])
+                self.recovered = np.array([point['recovered'] for point in data])
                 self.active = positive - self.recovered - self.dead
                 self.date_list = [point['date'] for point in data]
             except yaml.YAMLError as exc:
                 raise exc
 
         self.cumulative_active = list(
-            filter(lambda x: x >= country_basic.case_count,
-                   itertools.accumulate(self.active)))
-        self.date_list = self.date_list[len(self.active) -
-                                        len(self.cumulative_active):]
+            filter(lambda x: x >= country_basic.case_count, np.add.accumulate(self.active)))
+        self.date_list = self.date_list[len(self.active) - len(self.cumulative_active):]
         self.x = np.arange(1, len(self.cumulative_active) * 2)
         self.y = country_basic.formula.lambd(self.x)
 
         self.last_date = datetime.strptime(self.date_list[-1], '%Y-%m-%d')
-        self.date_list += [
-            (self.last_date + timedelta(days=d)).strftime('%Y-%m-%d')
-            for d in range(1,
-                           len(self.x) - len(self.date_list) + 1)
-        ]
+        self.date_list += [(self.last_date + timedelta(days=d)).strftime('%Y-%m-%d')
+                           for d in range(1,
+                                          len(self.x) - len(self.date_list) + 1)]
         self.x = self.x[:len(self.date_list)]
 
     def create_country_figure(self, graph_type=GraphType.SemiLog):
 
-        layout = Layout(
-            title=f"Active cases in {self.name}",
-            xaxis=dict(
-                autorange=True,
-                title=r'$\text{Days since the 200}^\mathrm{th}\text{ case}$'),
-            yaxis=dict(autorange=True, title='COVID-19 active cases'),
-            hovermode='x',
-            font={'size': 15},
-            legend=dict(x=0.01, y=0.99, borderwidth=1))
+        layout = Layout(title=f"Active cases in {self.name}",
+                        xaxis=dict(
+                            autorange=True,
+                            title=r'$\text{Days since the 200}^\mathrm{th}\text{ case}$',
+                            type='category',
+                            categoryorder='category ascending',
+                        ),
+                        yaxis=dict(autorange=True, title='COVID-19 active cases'),
+                        hovermode='x',
+                        font={'size': 15},
+                        legend=dict(x=0.01, y=0.99, borderwidth=1))
 
+        x = self.x if graph_type != GraphType.Normal else self.date_list
         figure = Figure(layout=layout)
         figure.add_trace(
-            Scatter(x=self.x,
-                    y=self.cumulative_active,
-                    mode='lines+markers',
-                    name=f"Active cases",
-                    line={'width': 3},
-                    marker={'size': 8}))
-
-        figure.add_trace(
             Scatter(
-                x=self.x,
+                x=x,
                 y=self.y,
                 text=self.date_list,
                 mode='lines',
                 name=self.formula.text,
                 line={
                     'dash': 'dash',
-                    'width': 3
+                    'width': 3,
+                    'color': 'rgb(31, 119, 180)',
                 },
             ))
+
+        figure.add_trace(
+            Scatter(x=x,
+                    y=self.cumulative_active,
+                    mode='lines+markers',
+                    name=f"Active cases",
+                    line={
+                        'width': 3,
+                        'color': 'rgb(239, 85, 59)',
+                    },
+                    marker={'size': 8}))
         if graph_type == GraphType.Normal:
             figure.update_yaxes(type="linear")
         elif graph_type == GraphType.SemiLog:
@@ -138,7 +129,7 @@ class CountryData:
 if args.country != "ALL":
     country = next(c for c in countries if c.name == args.country)
     country_data = CountryData(args.data, country)
-    country_data.create_country_figure().show()
+    country_data.create_country_figure(GraphType.Normal).show()
     # offline.plot(country_data.create_country_figure(), filename='graph.html')
 else:
     countries_data = [CountryData(args.data, country) for country in countries]
@@ -148,8 +139,7 @@ else:
     ])
 
     graphs = [
-        dcc.Graph(id=country_data.name,
-                  figure=country_data.create_country_figure())
+        dcc.Graph(id=country_data.name, figure=country_data.create_country_figure())
         for country_data in countries_data
     ]
 

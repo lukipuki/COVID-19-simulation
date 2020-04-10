a5041ccd7022c87018e2311c58c97689708e6c12
Lukáš Poláček <lukas@polacek.email>
2020-04-10 17:28:43 +0200

    Run multiple scenarios in simulation.cpp

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -11,17 +11,18 @@
 #include "simulation_results.pb.h"
 #include "stats.h"
 
+
+// #define EXPONENTIAL_GROWTH
+// constexpr bool kExponentialGrowth = true;
 constexpr bool kExponentialGrowth = false;
-constexpr uint32_t kExtraDays = 10;  // Extra simulated days of infections
-// constexpr uint32_t kRestrictionDay = 11;  // 0-indexed March 12th
-constexpr uint32_t kRestrictionDay = 8;  // For polynomial growth
+
+constexpr uint32_t kRestrictionDay = 8 + static_cast<int>(kExponentialGrowth) * 3;
 constexpr double kGamma1 = 1.25;
-constexpr double kGamma2 = 1.04;
-constexpr double kPolynomialDegree = 1.25;
 constexpr uint32_t kSymptomsLength = 28;
+constexpr uint32_t kExtraDays = 10;  // Extra simulated days of infections
 
 // Only serialize good parameters, scoring below kScoreThreshold
-constexpr double kScoreThreshold = 270;
+constexpr double kScoreThreshold = 280;
 
 class Simulator {
  public:
@@ -118,66 +119,74 @@ int main(int argc, char* argv[]) {
   }
   assert(tested.size() == positive.size());
 
-  // auto generator = ExponentialGenerator(kGamma1, kGamma2);
-  auto generator = PolynomialGenerator(kGamma1, kPolynomialDegree);
   constexpr uint32_t kIterations = 200;
   const uint32_t kEarlyStop = std::ceil(std::sqrt(kIterations));
   SimulationResults results;
   std::cout << "prefix_length optimal_b0 dead_count best_error" << std::endl;
 #pragma omp parallel for shared(positive, tested)
-  for (uint32_t prefix_length = 2; prefix_length < 19; ++prefix_length) {
-    Simulator simulator(prefix_length, positive, tested);
-    uint32_t optimal_b0 = -1, optimal_dead_count;
-    double best = 1e10;
-    for (uint32_t b0 = 10; b0 <= 200; b0 += 3) {
-      SimulationResult result;
-      result.set_prefix_length(prefix_length);
-      result.set_b0(b0);
-      std::vector<double> deltas = generator.CreateDeltas(
-          prefix_length + kRestrictionDay, prefix_length + tested.size() + kExtraDays);
-      *result.mutable_deltas() = {deltas.begin(), deltas.end()};
+#ifdef EXPONENTIAL_GROWTH
+// Yes, it's #ifdef, because 'if constexpr' sucks
+  for (uint32_t g = 96; g <= 106; g += 2) {
+    auto generator = ExponentialGenerator(kGamma1, g / 100.0);
+#else
+  for (uint32_t g = 122; g <= 130; g += 2) {
+    auto generator = PolynomialGenerator(kGamma1, g / 100.0);
+#endif
+    double param = g / 100.0;
+    for (uint32_t prefix_length = 1; prefix_length < 10; ++prefix_length) {
+      Simulator simulator(prefix_length, positive, tested);
+      uint32_t optimal_b0 = -1, optimal_dead_count;
+      double best = 1e10;
+      for (uint32_t b0 = 20; b0 <= 250; b0 += 3) {
+        SimulationResult result;
+        result.set_prefix_length(prefix_length);
+        result.set_b0(b0);
+        std::vector<double> deltas = generator.CreateDeltas(
+            prefix_length + kRestrictionDay, prefix_length + tested.size() + kExtraDays);
+        *result.mutable_deltas() = {deltas.begin(), deltas.end()};
 
-      if constexpr (kExponentialGrowth) {
-        result.set_gamma2(kGamma2);
-      } else {
-        result.set_alpha(kPolynomialDegree);
-      }
-      double sum_error = 0, dead_count = 0;
-      int iterations = 0;
-      for (uint32_t i = 0; i < kIterations; ++i) {
-        auto run = simulator.Simulate(b0, generator);
-        sum_error += run.error();
-        for (auto count : run.daily_dead()) {
-          dead_count += count;
+        if constexpr (kExponentialGrowth) {
+          result.set_gamma2(param);
+        } else {
+          result.set_alpha(param);
         }
-        *result.add_runs() = run;
-        ++iterations;
-        if (i == kEarlyStop && sum_error / iterations > 1.5 * kScoreThreshold) {
-          break;
+        double sum_error = 0, dead_count = 0;
+        int iterations = 0;
+        for (uint32_t i = 0; i < kIterations; ++i) {
+          auto run = simulator.Simulate(b0, generator);
+          sum_error += run.error();
+          for (auto count : run.daily_dead()) {
+            dead_count += count;
+          }
+          *result.add_runs() = run;
+          ++iterations;
+          if (i == kEarlyStop && sum_error / iterations > 1.5 * kScoreThreshold) {
+            break;
+          }
         }
+
+        sum_error /= iterations;
+        dead_count /= iterations;
+        if (sum_error < best) {
+          best = sum_error;
+          optimal_b0 = b0;
+          optimal_dead_count = dead_count;
+        }
+
+        SimulationResult::Summary summary;
+        summary.set_error(sum_error);
+        summary.set_dead_count(dead_count);
+        *result.mutable_summary() = summary;
+        if (sum_error > kScoreThreshold) {
+          result.mutable_deltas()->Clear();
+          result.mutable_runs()->Clear();
+        }
+        *results.add_results() = result;
       }
 
-      sum_error /= iterations;
-      dead_count /= iterations;
-      if (sum_error < best) {
-        best = sum_error;
-        optimal_b0 = b0;
-        optimal_dead_count = dead_count;
-      }
-
-      SimulationResult::Summary summary;
-      summary.set_error(sum_error);
-      summary.set_dead_count(dead_count);
-      *result.mutable_summary() = summary;
-      if (sum_error > kScoreThreshold) {
-        result.mutable_deltas()->Clear();
-        result.mutable_runs()->Clear();
-      }
-      *results.add_results() = result;
+      std::cout << std::setw(2) << prefix_length << std::setw(4) << optimal_b0 << std::setw(4)
+                << optimal_dead_count << std::setw(9) << best << std::endl;
     }
-
-    std::cout << std::setw(2) << prefix_length << std::setw(4) << optimal_b0 << std::setw(4)
-              << optimal_dead_count << std::setw(9) << best << std::endl;
   }
 
   std::fstream output("results.pb", std::ios::out | std::ios::trunc | std::ios::binary);

ea32682df2908f46615a6da1269166872113267d
Lukáš Poláček <lukas@polacek.email>
2020-04-13 20:47:02 +0200

    Convert country to entry_points

--- covid_graphs/setup.py
+++ covid_graphs/setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         "console_scripts": [
             "covid_graphs.run_server = covid_graphs.scripts.server:run_server",
+            "covid_graphs.country = covid_graphs.country:main",
             "covid_graphs.scatter_plot = covid_graphs.scatter_plot:main",
             "covid_graphs.prepare_data = covid_graphs.prepare_data:main"
         ]

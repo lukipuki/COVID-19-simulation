f04e05fcefd241d3b28c4a080d1a2d944f98d72e
Michal Szabados <misko.sz@gmail.com>
2020-04-14 21:35:49 +0100

    Use pathlib with Country graph. Polish the code a little.

--- covid_graphs/covid_graphs/country_report.py
+++ covid_graphs/covid_graphs/country_report.py
@@ -1,24 +1,36 @@
-import numpy as np
-import os
+from dataclasses import dataclass
 from google.protobuf import text_format
+import numpy as np
+from pathlib import Path
+from typing import List
+
+from .formula import Formula
 from .pb.country_data_pb2 import CountryData
 
 
+@dataclass
+class Country:
+    name: str
+    formulas: List[Formula]
+
+
 class CountryReport:
-    """Constructs a numpy representation of data read from 'data_dir' a given country."""
-    def __init__(self, data_dir, country_tuple):
-        with open(os.path.join(data_dir, f'{country_tuple.name}.data'), "rb") as f:
-            country_data = CountryData()
-            text_format.Parse(f.read(), country_data)
-            self.name = country_data.name
-            daily_positive = np.array([day.positive for day in country_data.stats])
-            self.daily_dead = np.array([day.dead for day in country_data.stats])
-            self.daily_recovered = np.array([day.recovered for day in country_data.stats])
-            self.daily_active = daily_positive - self.daily_recovered - self.daily_dead
-            self.date_list = [
-                f"{day.date.year}-{day.date.month:02d}-{day.date.day:02d}"
-                for day in country_data.stats
-            ]
+    def __init__(self, data_dir: Path, country: Country):
+        """Constructs a numpy representation of data read from 'data_dir' a given country."""
+        country_data = CountryData()
+        data_file = Path(data_dir / f'{country.name}.data')
+        text_format.Parse(data_file.read_text(), country_data)
+
+        self.name = country.name
+        self.date_list = [
+            f"{day.date.year}-{day.date.month:02d}-{day.date.day:02d}"
+            for day in country_data.stats
+        ]
+
+        daily_positive = np.array([day.positive for day in country_data.stats])
+        self.daily_dead = np.array([day.dead for day in country_data.stats])
+        self.daily_recovered = np.array([day.recovered for day in country_data.stats])
+        self.daily_active = daily_positive - self.daily_recovered - self.daily_dead
 
         self.cumulative_active = np.add.accumulate(self.daily_active)
-        self.min_case_count = min(formula.min_case_count for formula in country_tuple.formulas)
+        self.min_case_count = min(formula.min_case_count for formula in country.formulas)

6c8c793fda92f9eefc95a83940d4e7d01180b473
Lukáš Poláček <lukas@polacek.email>
2020-04-15 19:06:12 +0200

    Add prediction formulas

--- covid_graphs/covid_graphs/predictions.py
+++ covid_graphs/covid_graphs/predictions.py
@@ -20,7 +20,7 @@ class CountryPrediction:
 
 
 BK_20200329 = PredictionEvent(name="bk_20200329", date=datetime.date(2020, 3, 29))
-BK_20200413 = PredictionEvent(name="bk_20200413", date=datetime.date(2020, 4, 13))
+BK_20200412 = PredictionEvent(name="bk_20200412", date=datetime.date(2020, 4, 12))
 OTHER = PredictionEvent(name="other", date=datetime.date(2020, 4, 1))
 
 _prediction_database = [
@@ -58,31 +58,121 @@ _prediction_database = [
         formula=ATG_formula(7.2, 2719, 6.23),
     ),
 
-    # BK_20200413
+    # BK_20200412
     CountryPrediction(
-        prediction_event=BK_20200413,
-        country="Italy",
-        formula=ATG_formula(9.67, 30080, 5.26, 200),
+        prediction_event=BK_20200412,
+        country="Korea",
+        formula=ATG_formula(8.9, 79720, 2.52, 171),
     ),
     CountryPrediction(
-        prediction_event=BK_20200413,
-        country="USA",
-        formula=ATG_formula(12.8, 1406000, 4.3, 1083),
+        prediction_event=BK_20200412,
+        country="Austria",
+        formula=ATG_formula(3.48, 2.13, 8.47, 29),
     ),
     CountryPrediction(
-        prediction_event=BK_20200413,
-        country="Spain",
-        formula=ATG_formula(5.93, 1645, 6.54, 155),
+        prediction_event=BK_20200412,
+        country="Iceland",
+        formula=ATG_formula(5.58, 47.8, 6.01, 10),
     ),
     CountryPrediction(
-        prediction_event=BK_20200413,
+        prediction_event=BK_20200412,
+        country="Jordan",
+        formula=ATG_formula(7.99, 3121, 2.18, 10),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Switzerland",
+        formula=ATG_formula(4.19, 16.65, 7.78, 28),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Australia",
+        formula=ATG_formula(4.97, 426.75, 5.59, 83),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Croatia",
+        formula=ATG_formula(8.5, 1608, 4.23, 14),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
         country="Germany",
         formula=ATG_formula(5.99, 5086, 5.79, 274),
     ),
     CountryPrediction(
-        prediction_event=BK_20200413,
-        country="Switzerland",
-        formula=ATG_formula(4.19, 16.65, 7.78, 28),
+        prediction_event=BK_20200412,
+        country="Israel",
+        formula=ATG_formula(3.99, 0.32, 9.40, 29),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Italy",
+        formula=ATG_formula(9.67, 30080, 5.26, 200),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Malaysia",
+        formula=ATG_formula(13.37, 46882, 2.38, 104),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="New Zealand",
+        formula=ATG_formula(2.96, 0.00024, 11.36, 4),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Spain",
+        formula=ATG_formula(5.93, 1645, 6.54, 155),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Belgium",
+        formula=ATG_formula(8.33, 2838, 5.64, 38),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Canada",
+        formula=ATG_formula(13.3, 117591, 3.5, 123),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Chile",
+        formula=ATG_formula(18.8, 152200, 2.5, 62),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Czechia",
+        formula=ATG_formula(13.1, 40610, 3.3, 35),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Latvia",
+        formula=ATG_formula(17.4, 16680, 2.3, 10),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Lithuania",
+        formula=ATG_formula(8.2, 822, 4.5, 1),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Netherlands",
+        formula=ATG_formula(16.7, 157180, 3.8, 57),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Norway",
+        formula=ATG_formula(15.5, 48762, 3.4, 18),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="Portugal",
+        formula=ATG_formula(11.2, 31462, 4.28, 34),
+    ),
+    CountryPrediction(
+        prediction_event=BK_20200412,
+        country="USA",
+        formula=ATG_formula(12.8, 1406478, 4.3, 1083),
     ),
 ]
 
--- data/download_all.zsh
+++ data/download_all.zsh
@@ -3,11 +3,18 @@
 declare -A short_names
 short_names["US"]=USA
 short_names["United Kingdom"]=UK
+short_names["New Zealand"]=NZ
 
 python3.7 -m venv --system-site-packages venv && source venv/bin/activate
 pip install $1/
-for country in Germany Iran Italy Spain "United Kingdom" US
+
+#Korea Australia Canada 
+countries=("United Kingdom" Austria Iceland Jordan Switzerland Croatia
+  Germany Israel Italy Malaysia "New Zealand" Spain Belgium Chile Czechia Latvia
+  Lithuania Netherlands Norway Portugal US)
+for country in $countries
 do
+  echo $country
   if [[ -z $short_names["${country}"] ]]; then
     covid_graphs.prepare_data $country
   else

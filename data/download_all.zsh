f227ff04fc850896ebe45c0fa77753d5e4da6e47
Lukáš Poláček <lukas@polacek.email>
2020-04-15 06:59:26 +0200

    Update data, add Switzerland

--- data/download_all.zsh
+++ data/download_all.zsh
@@ -4,9 +4,9 @@ declare -A short_names
 short_names["US"]=USA
 short_names["United Kingdom"]=UK
 
-python3.7 -m venv venv && source venv/bin/activate
+python3.7 -m venv --system-site-packages venv && source venv/bin/activate
 pip install $1/
-for country in France Germany Iran Italy Spain "United Kingdom" US
+for country in Germany Iran Italy Spain "United Kingdom" US
 do
   if [[ -z $short_names["${country}"] ]]; then
     covid_graphs.prepare_data $country

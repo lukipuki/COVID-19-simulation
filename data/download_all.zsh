2b7ed57371588edb78d358cb5bd08cd768caec78
Lukáš Poláček <lukas@polacek.email>
2020-04-15 19:43:15 +0200

    Fix data for countries with territories

--- data/download_all.zsh
+++ data/download_all.zsh
@@ -1,24 +1,22 @@
 #!/bin/zsh -xe
 
 declare -A short_names
-short_names["US"]=USA
+short_names["United States"]=USA
 short_names["United Kingdom"]=UK
 short_names["New Zealand"]=NZ
 
 python3.7 -m venv --system-site-packages venv && source venv/bin/activate
 pip install $1/
 
-#Korea Australia Canada 
-countries=("United Kingdom" Austria Iceland Jordan Switzerland Croatia
+countries=("United Kingdom" Korea Austria Iceland Jordan Switzerland Croatia Australia Canada
   Germany Israel Italy Malaysia "New Zealand" Spain Belgium Chile Czechia Latvia
   Lithuania Netherlands Norway Portugal US)
 for country in $countries
 do
-  echo $country
-  if [[ -z $short_names["${country}"] ]]; then
-    covid_graphs.prepare_data $country
+  if [[ -z $short_names[${(p)country}] ]]; then
+    covid_graphs.prepare_data ${(p)country}
   else
-    covid_graphs.prepare_data $country --short_name $short_names["${country}"]
+    covid_graphs.prepare_data ${(p)country} --short_name $short_names[${(p)country}]
   fi
 done
 

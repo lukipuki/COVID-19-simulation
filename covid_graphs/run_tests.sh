f97346c7fbeb9e7dbc44ded5a9894499d8401c72
Michal Szabados <misko.sz@gmail.com>
2020-04-19 22:04:55 +0100

    Format and lint code.

--- covid_graphs/run_tests.sh
+++ covid_graphs/run_tests.sh
@@ -6,8 +6,17 @@ then
     echo "⚠️  Running outside of a virtual environment."
 fi
 
-echo "👮 Type checking.."
+echo "📝 Checking order of imports..."
+isort covid_graphs/ --check-only --recursive --diff
+
+echo "⚫ Checking code format..."
+black covid_graphs/ --diff --check
+
+echo "👮 Type checking..."
 mypy covid_graphs/
+
+echo "🧶 Linting..."
+flake8 --config setup.cfg --jobs auto covid_graphs/
 
 echo "🏃 Running tests..."
 pytest

147f38b09387f2becc84847ce797b071ca2f1fc7
Martin Rejda <rejdi@ksp.sk>
2020-05-01 23:42:43 +0200

    Moved nginx to /web. Updated README.md for covid_graphs. Added README.md for web. Updated docker-compose.yml

--- web/covid_web/server.py
+++ web/covid_web/server.py
@@ -37,13 +37,11 @@ def run_server(data_dir: Path) -> None:
     """Creates and runs a flask server."""
     server = Flask(__name__, template_folder=str(CURRENT_DIR))
 
-    _setup_server(server, data_dir)
-
     if platform_supports_inotify:
         i = adapters.InotifyTree(
             str(data_dir), mask=(constants.IN_MODIFY | constants.IN_DELETE | constants.IN_CREATE)
         )
-        p = Process(target=_run_flask_server, kwargs=dict(server=server))
+        p = Process(target=_run_flask_server, kwargs=dict(server=server, data_dir=data_dir))
         p.start()
         while True:
             events = list(i.event_gen(yield_nones=False, timeout_s=1))
@@ -51,7 +49,7 @@ def run_server(data_dir: Path) -> None:
                 print("Data changed. Restarting server...")
                 p.terminate()
                 p.join()
-                p = Process(target=_run_flask_server, kwargs=dict(server=server))
+                p = Process(target=_run_flask_server, kwargs=dict(server=server, data_dir=data_dir))
                 p.start()
                 print("Server restarted.")
             sleep(10)
@@ -84,7 +82,8 @@ def _setup_server(server: Flask, data_dir: Path):
     _create_rest(server=server, data_dir=data_dir)
 
 
-def _run_flask_server(server: Flask):
+def _run_flask_server(server: Flask, data_dir: Path):
+    _setup_server(server, data_dir)
     server.run(host="0.0.0.0", port=8081)
 
 

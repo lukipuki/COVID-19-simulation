9bab1867d0e900db0962ddb87476dca282f1ed1f
Martin Rejda <4532544+rejdi@users.noreply.github.com>
2020-05-01 22:25:12 +0200

    Move Python web (#77)
    Co-authored-by: Lukáš Poláček <lukas@polacek.email>

--- web/covid_web/wsgi.py
+++ web/covid_web/wsgi.py
@@ -1,10 +1,11 @@
 from os import getenv
 from pathlib import Path
 
-from covid_graphs.scripts.server import uswgi_server
+from .server import uswgi_server
 
 data_path = getenv(key="DATA_PATH", default="data")
 app = uswgi_server(Path(data_path))
+# app = setup_server(Path(data_path))
 
 if __name__ == "__main__":
     app.run()
---
+++ web/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+setup(
+    name="covid_web",
+    version="1.0",
+    description="COVID-19 web apps",
+    install_requires=[
+        "click",
+        "click_pathlib",
+        "covid_graphs",
+        "dash",
+        "Flask",
+    ],
+    dependency_links=[],
+    python_requires=">=3.7",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "covid_web.run_server = covid_web.server:run_server",
+        ]
+    },
+    package_data={"": ["*.html"]},
+)

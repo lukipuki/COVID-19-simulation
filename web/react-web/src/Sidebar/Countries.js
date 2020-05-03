28c9cc1c5e87940a1320a0fdbd6552858d6578b7
Martin Rejda <rejdi@ksp.sk>
2020-05-03 20:44:48 +0200

    Comment cleanup. Sanity check on input data. Better code formatting in rendering function.

--- web/react-web/src/Sidebar/Countries.js
+++ web/react-web/src/Sidebar/Countries.js
@@ -25,10 +25,12 @@ class Countries extends Component {
         const countries = new Set();
         const predictionDates = Object.keys(predictions.data).sort();
         for (let dateKey in predictions.data) {
-            const prediction = predictions.data[dateKey];
-            prediction.countries.forEach(country => {
-                countries.add(country);
-            });
+            if (predictions.data.hasOwnProperty(dateKey)) {
+                const prediction = predictions.data[dateKey];
+                prediction.countries.forEach(country => {
+                    countries.add(country);
+                });
+            }
         }
         const countryList = [...countries].sort().map((country, index) => {
             const checks = predictionDates.map((prediction, i) => {
@@ -49,34 +51,19 @@ class Countries extends Component {
             return <tr key={index}><td><button className={className} onClick={this.toggleItem(countryKey)}>{country}</button></td>{checks}</tr>
         });
 
-
-        // if (!predictionsContext.data.hasOwnProperty(this.props.selectedDateKey)) {
-        //     return null; //Loader
-        // }
-        // const predictionsToDate = predictionsContext.data[this.props.selectedDateKey];
-        //
-        // const countries = predictionsToDate.countries.map((country, i)=> {
-        //         //a bit hacky to use part of api path as key
-        //         const prediction = `${this.props.selectedDateKey}/${country}`;
-        //         const active = selectionsContext.selections.hasOwnProperty(prediction);
-        //         const className = `country${active ? ' active' : ''}`;
-        //         return <button key={i} className={className} onClick={this.togglePrediction(prediction)}>{country}</button>
-        //     }
-        // );
-        //
-        // return <div className='buttons'>{countries}</div>;
-
-        return <table>
-            <thead>
-                <tr>
-                    <th rowSpan={2}>Country<br/>(active cases)</th><th colSpan={predictionDates.length}>Predictions</th>
-                </tr>
-                <tr>
-                    {predictionDates.map((value, index) => <th key={index}>{new Date(Date.parse(predictions.data[value].label)).toLocaleDateString()}</th>)}
-                </tr>
-            </thead>
-            <tbody>{countryList}</tbody>
-        </table>;
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th rowSpan={2}>Country<br/>(active cases)</th><th colSpan={predictionDates.length}>Predictions</th>
+                    </tr>
+                    <tr>
+                        {predictionDates.map((value, index) => <th key={index}>{new Date(Date.parse(predictions.data[value].label)).toLocaleDateString()}</th>)}
+                    </tr>
+                </thead>
+                <tbody>{countryList}</tbody>
+            </table>
+        );
     }
 }
 export default Countries;

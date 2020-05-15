d504e754690b71ee0e05289460305e1a51624680
Martin Rejda <4532544+rejdi@users.noreply.github.com>
2020-05-16 00:02:01 +0200

    React web active cases scaling (#122)
    * Added ability to scale data by capita, absolute or same peak.
    
    Co-authored-by: Martin Rejda <rejdi@ksp.sk>
    Co-authored-by: Lukáš Poláček <lukas@polacek.email>

--- web/react-web/src/Sidebar/index.js
+++ web/react-web/src/Sidebar/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {GraphDetailContext, AXES_LINEAR, AXES_LOG, AXES_LOG_LOG} from "../Commons/sharedObjects";
+import {
+    GraphDetailContext, AXES_LINEAR, AXES_LOG, AXES_LOG_LOG, SCALING_PER_CAPITA,
+    SCALING_ABSOLUTE, SCALING_SAME_PEAK
+} from "../Commons/sharedObjects";
 import CountriesWrapper from "./CountriesWrapper";
 import ReactMarkdown from "react-markdown/with-html";
 import {api} from "../Commons/api";
@@ -27,19 +30,19 @@ class Sidebar extends Component {
         this.setState({activeTab})
     };
 
-    changeGraphDetail = (axesType, isXAxisRelative) => () => {
+    changeGraphDetail = (axesType, isXAxisRelative, dataScaling) => () => {
         const {setOptions} = this.context;
         if (!isXAxisRelative && axesType === AXES_LOG_LOG) {
             axesType = AXES_LINEAR;
         }
-        setOptions({axesType, isXAxisRelative});
+        setOptions({axesType, isXAxisRelative, dataScaling});
     };
 
     render() {
         return (
             <GraphDetailContext.Consumer>
                 {graphDetailContext => {
-                    const {isXAxisRelative, axesType} = graphDetailContext.options;
+                    const {isXAxisRelative, axesType, dataScaling} = graphDetailContext.options;
                     const {activeTab} = this.state;
 
                     return (
@@ -53,16 +56,49 @@ class Sidebar extends Component {
                                     <fieldset>
                                         <legend>Show by:</legend>
                                         <div className='buttons'>
-                                            <button onClick={this.changeGraphDetail(axesType, true)} className={isXAxisRelative ? 'active' : ''}>Day</button>
-                                            <button onClick={this.changeGraphDetail(axesType, false)} className={!isXAxisRelative ? 'active' : ''}>Date</button>
+                                            <button onClick={this.changeGraphDetail(axesType, true, dataScaling)} className={isXAxisRelative ? 'active' : ''}>Day</button>
+                                            <button onClick={this.changeGraphDetail(axesType, false, dataScaling)} className={!isXAxisRelative ? 'active' : ''}>Date</button>
+                                        </div>
+                                    </fieldset>
+                                    <fieldset>
+                                        <legend>Active cases:</legend>
+                                        <div className='buttons'>
+                                            <button
+                                                onClick={this.changeGraphDetail(axesType, isXAxisRelative, SCALING_PER_CAPITA)}
+                                                className={dataScaling === SCALING_PER_CAPITA ? 'active' : ''}>
+                                                Per capita
+                                            </button>
+                                            <button
+                                                onClick={this.changeGraphDetail(axesType, isXAxisRelative, SCALING_ABSOLUTE)}
+                                                className={dataScaling === SCALING_ABSOLUTE ? 'active' : ''}>
+                                                Absolute
+                                            </button>
+                                            <button
+                                                onClick={this.changeGraphDetail(axesType, isXAxisRelative, SCALING_SAME_PEAK)}
+                                                className={dataScaling === SCALING_SAME_PEAK ? 'active' : ''}>
+                                                Same peak
+                                            </button>
                                         </div>
                                     </fieldset>
                                     <fieldset>
                                         <legend>Axes adjustments:</legend>
                                         <div className='buttons'>
-                                            <button onClick={this.changeGraphDetail(AXES_LINEAR, isXAxisRelative)} className={axesType === AXES_LINEAR ? 'active' : ''}>Linear</button>
-                                            <button onClick={this.changeGraphDetail(AXES_LOG, isXAxisRelative)} className={axesType === AXES_LOG ? 'active' : ''}>Log Y</button>
-                                            <button disabled={!isXAxisRelative} onClick={this.changeGraphDetail(AXES_LOG_LOG, isXAxisRelative)} className={axesType === AXES_LOG_LOG ? 'active' : ''}>Log XY</button>
+                                            <button
+                                                onClick={this.changeGraphDetail(AXES_LINEAR, isXAxisRelative, dataScaling)}
+                                                className={axesType === AXES_LINEAR ? 'active' : ''}>
+                                                Linear
+                                            </button>
+                                            <button
+                                                onClick={this.changeGraphDetail(AXES_LOG, isXAxisRelative, dataScaling)}
+                                                className={axesType === AXES_LOG ? 'active' : ''}>
+                                                Log Y
+                                            </button>
+                                            <button
+                                                disabled={!isXAxisRelative}
+                                                onClick={this.changeGraphDetail(AXES_LOG_LOG, isXAxisRelative, dataScaling)}
+                                                className={axesType === AXES_LOG_LOG ? 'active' : ''}>
+                                                Log XY
+                                            </button>
                                         </div>
                                     </fieldset>
                                     <fieldset>
